676d2cff8fafb87a8f2b13e4582a8662fb595a39
toumarihikana <toumarihikana@users.noreply.github.com>
2022-05-30 23:47:55 +0000

    deploy: 19b8431a17c63ce0905ac87f8eeb65489cc0052f

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,14 +3,14 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "main.dart.js": "13eae3ce177d8c4b1953fed8478baf51",
+  "main.dart.js": "80d8e33f604abc77621ee76b4e54f2c0",
 "canvaskit/canvaskit.wasm": "4b83d89d9fecbea8ca46f2f760c5a9ba",
 "canvaskit/profiling/canvaskit.wasm": "95e736ab31147d1b2c7b25f11d4c32cd",
 "canvaskit/profiling/canvaskit.js": "ae2949af4efc61d28a4a80fffa1db900",
 "canvaskit/canvaskit.js": "c2b4e5f3d7a3d82aed024e7249a78487",
 "flutter.js": "0816e65a103ba8ba51b174eeeeb2cb67",
-"index.html": "932add194f767e886ff0aa28a3846539",
-"/": "932add194f767e886ff0aa28a3846539",
+"index.html": "7d1f3b02032605c8e07c645a4a3d40fa",
+"/": "7d1f3b02032605c8e07c645a4a3d40fa",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "version.json": "ea069aab3819cd828653e73b40f20397",
 "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
